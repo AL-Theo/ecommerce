cb68a650eea27527879e5f70594cf98f79964f58
Thiago Almeida <43508221+AL-Theo@users.noreply.github.com>
2019-12-23 21:04:59 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 //Sample on how to build DB security on serverside
 module.exports = {
   env: {
-    MONGO_SRV: "mongodb+srv://ThiagoAlm:my123@cluster0-dkixg.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "jwtsecrettoken",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/thiagoa/image/upload",
-    STRIPE_SECRET_KEY: "sk_test_Yc5W7RRMMrtc4rrPW7kbGE6L00h6gqvwLp"
+    MONGO_SRV: "mongodb+srv://name:pw@cluster0-dkixg.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "yourtoken",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/name/image/upload",
+    STRIPE_SECRET_KEY: "yourstripekey"
   }
 };
 
